7bed860a2c116d43284bf8e091e1c12fd4e251f2
MukhriddinMike <miwady@mail.ru>
2019-02-02 15:55:18 +0900

    2/2/2019

---
+++ Udemy-Full-Python/codes/class.py
@@ -0,0 +1,21 @@
+
+class Enemy:
+
+    def __init__(self,atkl,atkh):
+        self.atkl=atkl
+        self.atkh=atkh
+
+    def getAtk(self):
+        print(self.atkl)
+
+    def getAtkH(self):
+        print(self.atkh)
+
+
+enemy1 = Enemy(40,49)
+enemy1.getAtk()
+
+enemy2 = Enemy(75,90)
+enemy2.getAtk()
+
+enemy2.getAtkH()
